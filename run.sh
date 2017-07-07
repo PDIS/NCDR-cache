c2b67a23e91bbb66d5753229a20270975bd0e661
mark19891107 <mark19891107@gmail.com>
2017-07-07 14:53:27 +0800

    use shell script

---
+++ run.sh
@@ -0,0 +1,21 @@
+chrome-har-capturer \
+-o light.har \
+-g 10000 \
+-c "http://itwapi.ncdr.nat.gov.tw/web/apiIndex.aspx?PageId=221&Cx=121&Cy=23.5&Scale=3&Info=Y&MapControl=Y&AddrLoc=Y&Token=YuDz2cxPv9XJ"
+
+go run harx.go -x light light.har
+
+mv light/itwapi.ncdr.nat.gov.tw/web/apiIndex.aspx light/itwapi.ncdr.nat.gov.tw/web/index.html
+
+mv light/223.200.166.9/* light/itwapi.ncdr.nat.gov.tw/
+mv light/223.200.166.10/* light/itwapi.ncdr.nat.gov.tw/
+
+
+find light -type f -exec sed -i "s/type: 'post'/type: 'get'/g" {} +
+
+find light -type f -exec sed -i "s/http:\/\/223.200.166.9//g" {} +
+
+find light -type f -exec sed -i "s/http:\/\/223.200.166.10//g" {} +
+
+find light -type f -exec sed -i "s/(location.protocol === 'file:' ? 'http:' : location.protocol) + '\/\/' + \"223.200.166.10/\"/g" {} +
+
