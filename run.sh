5227b979fa662c032942d13079c10f712c703879
Mark Dai <mark19891107@users.noreply.github.com>
2017-07-10 11:41:49 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,22 +3,16 @@ chrome-har-capturer \
 -g 10000 \
 -c "http://itwapi.ncdr.nat.gov.tw/web/apiIndex.aspx?PageId=221&Cx=121&Cy=23.5&Scale=3&Info=Y&MapControl=Y&AddrLoc=Y&Token=YuDz2cxPv9XJ"
 
-ls
-
 git clone https://github.com/outersky/har-tools.git
 
 go run har-tools/harx.go -x light light.har
 
 mv light/itwapi.ncdr.nat.gov.tw/web/apiIndex.aspx light/itwapi.ncdr.nat.gov.tw/web/index.html
-
 mv light/223.200.166.9/* light/itwapi.ncdr.nat.gov.tw/
 mv light/223.200.166.10/* light/itwapi.ncdr.nat.gov.tw/
 
-
 find light -type f -exec sed -i "s/type: 'post'/type: 'get'/g" {} +
-
 find light -type f -exec sed -i "s/http:\/\/223.200.166.9//g" {} +
-
 find light -type f -exec sed -i "s/http:\/\/223.200.166.10//g" {} +
-
+find light -type f -exec sed -i "s/\/ArcGIS_JSAPI\/3.16\/init.js/https:\/\/js.arcgis.com\/3.16\/init.js/g" {} +
 find light -type f -exec sed -i "s/(location.protocol === 'file:' ? 'http:' : location.protocol) + '\/\/' + \"223.200.166.10/\"/g" {} +
